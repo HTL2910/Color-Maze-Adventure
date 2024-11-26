c3bd8e1ffd36ed17d55101285dc4f12188ba0a45
HTL2910 <hathanhlong2910@gmail.com>
2024-11-26 18:37:42 +0700

    Update BoolMatrixEditor.cs

--- Assets/Editor/BoolMatrixEditor.cs
+++ Assets/Editor/BoolMatrixEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(BoolMatrix))]
@@ -34,6 +35,30 @@ public class BoolMatrixEditor : Editor
             {
                 Debug.LogWarning("Clipboard does not contain valid data.");
             }
+        }
+        boolMatrix.rows = EditorGUILayout.IntField("Rows", boolMatrix.rows);
+        boolMatrix.columns = EditorGUILayout.IntField("Columns", boolMatrix.columns);
+
+        // Đảm bảo ma trận khớp với kích thước
+        if (boolMatrix.matrix.Count != boolMatrix.rows * boolMatrix.columns)
+        {
+            boolMatrix.matrix = new List<bool>(new bool[boolMatrix.rows * boolMatrix.columns]);
+        }
+
+        // Hiển thị ma trận dưới dạng bảng
+        for (int i = 0; i < boolMatrix.rows; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            for (int j = 0; j < boolMatrix.columns; j++)
+            {
+                bool currentValue = boolMatrix.GetValue(i, j);
+                bool newValue = EditorGUILayout.Toggle(currentValue, GUILayout.Width(20));
+                if (currentValue != newValue)
+                {
+                    boolMatrix.SetValue(i, j, newValue);
+                }
+            }
+            EditorGUILayout.EndHorizontal();
         }
 
         // Lưu thay đổi
